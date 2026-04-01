5182351ac7f6a8db9a4d8daa000584cee47699bd
Sebas <96390947+SebastianLuser@users.noreply.github.com>
2026-04-01 09:45:14 -0300

    Update src/core/entities/course.go
    Co-authored-by: Jose Attento <65502562+jattento@users.noreply.github.com>

--- src/core/entities/course.go
+++ src/core/entities/course.go
@@ -4,7 +4,7 @@ import "time"
 
 type Course struct {
 	ID             int64     `json:"id" gorm:"primaryKey"`
-	OrganizationID int64     `json:"organization_id"`
+	OrganizationID uuid.UUID     `json:"organization_id"`
 	Name           string    `json:"name"`
 	Year           int       `json:"year"`
 	TimeTrackedEntity
