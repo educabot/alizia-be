64eeb2cc9b9bf17b1bf306331904a4bb5dc41d46
SebastianLuser <sebastianv.08.2000@gmail.com>
2026-04-07 14:21:21 -0300

    fix testing

---
+++ src/mocks/providers/organization.go
@@ -0,0 +1,30 @@
+package providers
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/educabot/alizia-be/src/core/entities"
+)
+
+type MockOrganizationProvider struct {
+	mock.Mock
+}
+
+func (m *MockOrganizationProvider) FindByID(ctx context.Context, id uuid.UUID) (*entities.Organization, error) {
+	args := m.Called(ctx, id)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*entities.Organization), args.Error(1)
+}
+
+func (m *MockOrganizationProvider) FindBySlug(ctx context.Context, slug string) (*entities.Organization, error) {
+	args := m.Called(ctx, slug)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*entities.Organization), args.Error(1)
+}
